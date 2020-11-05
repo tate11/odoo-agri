4040c171b89d6d486d5325365ddcb217b40a32d3
Helmut Drewes <helmut.drewes@agrista.com>
2020-11-05 11:27:50 +0200

    [IMP] agri: Add cost type to product cateories

--- agri/models/product.py
+++ agri/models/product.py
@@ -6,6 +6,23 @@ class ProductCategory(models.Model):
     _inherit = 'product.category'
 
     is_agri = fields.Boolean('Is Agri', default=False)
+    cost_type = fields.Selection(
+        [
+            ('crop_sale', 'Crop Sales'),
+            ('permanent_crop_sale', 'Permanent Crop Sale'),
+            ('crop_establishment', 'Crop Establishment'),
+            ('crop_input', 'Crop Input'),
+            ('crop_harvest', 'Crop Harvest'),
+            ('livestock_sale', 'Livestock Sale'),
+            ('livestock_product', 'Livestock Product'),
+            ('animal_feed', 'Animal Feed'),
+            ('husbandry', 'Husbandry'),
+            ('livestock_harvest', 'Livestock Harvest'),
+            ('overhead', 'Overhead'),
+        ],
+        string='Cost Type',
+        default='crop_sale',
+    )
     uom_id = fields.Many2one('uom.uom',
                              'Unit of Measure',
                              help='Unit of measure used for allocating costs.')
@@ -15,4 +32,9 @@ class ProductCategory(models.Model):
     @api.constrains('uom_id')
     def _check_uom_id(self):
         if self.is_agri and not self.uom_id:
-            raise ValidationError(_('Unit of Measure must be set.'))
+            raise ValidationError(_('Unit of Measure must be set agricultural product categories'))
+
+    @api.constrains('cost_type')
+    def _check_uom_id(self):
+        if self.is_agri and not self.cost_type:
+            raise ValidationError(_('The cost type must be set for agricultural product categories'))
