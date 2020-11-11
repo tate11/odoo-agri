97d625dd63339afc6a34d2b9af484e8ea64cb15b
Ross Savage <ross.savage@agrista.com>
2020-11-11 15:55:51 +0100

    [IMP] agri_farm: Improve farm version handling

--- agri_farm/models/agri_farm.py
+++ agri_farm/models/agri_farm.py
@@ -23,7 +23,7 @@ class Farm(models.Model):
     area_ha = fields.Float('Hectares', digits='Hectare')
     boundary = fields.GeoPolygon('Boundary', srid=4326, gist_index=True)
     has_boundary = fields.Boolean('Has Boundary',
-                                  computed='_compute_has_boundary',
+                                  compute='_compute_has_boundary',
                                   default=False)
     farm_field_ids = fields.One2many(comodel_name='agri.farm.field',
                                      inverse_name='farm_id',
@@ -56,8 +56,8 @@ class Farm(models.Model):
 
     @api.onchange('partner_id')
     def onchange_partner_id(self):
-        if self.partner_id:
-            self.farm_version_id = self.partner_id.farm_version_id
+        for farm in self:
+            farm.farm_version_id = farm.partner_id.farm_version_id if farm.partner_id else False
 
     def name_get(self):
         return [(farm.id, "{} ({:.3f} ha)".format(farm.name, farm.area_ha))
@@ -75,14 +75,15 @@ class FarmField(models.Model):
     area_ha = fields.Float('Hectares', digits='Hectare', tracking=True)
     boundary = fields.GeoPolygon('Boundary', srid=4326, gist_index=True)
     has_boundary = fields.Boolean('Has Boundary',
-                                  computed='_compute_has_boundary',
+                                  compute='_compute_has_boundary',
                                   default=False)
     established_date = fields.Date('Established Date', tracking=True)
     farm_id = fields.Many2one('agri.farm',
                               'Farm',
                               ondelete='cascade',
                               required=True)
-
+    farm_version_id = fields.Many2one(related='farm_id.farm_version_id',
+                                      readonly=True)
     crop_potential_id = fields.Many2one('agri.crop.potential',
                                         'Crop Potential',
                                         tracking=True)
@@ -112,19 +113,21 @@ class FarmField(models.Model):
 
     @api.constrains('name')
     def constrains_name(self):
-        domain = [('farm_id', '=', self.farm_id.id), ('active', '=', True),
-                  ('name', 'ilike', self.name)]
-        if self.id:
-            domain.append(('id', '!=', self.id))
-        field = self.env['agri.farm.field'].search(domain, limit=1)
-        if field:
-            raise ValidationError(_('Duplicate Field name'))
+        for farm in self:
+            domain = [('farm_id', '=', farm.farm_id.id), ('active', '=', True),
+                    ('name', 'ilike', farm.name)]
+            if farm.id:
+                domain.append(('id', '!=', farm.id))
+            field = self.env['agri.farm.field'].search(domain, limit=1)
+            if field:
+                raise ValidationError(_('Duplicate Field name'))
 
     @api.onchange('irrigation_type_id', 'land_use_id')
     def onchange_irrigated(self):
-        self.irrigated = True if (self.land_use_id
-                                  and self.land_use_id.irrigated
-                                  ) or self.irrigation_type_id else False
+        for field in self:
+            field.irrigated = True if (field.land_use_id
+                                       and field.land_use_id.irrigated
+                                       ) or field.irrigation_type_id else False
 
     def name_get(self):
         return [(field.id, "{} ({:.3f} ha)".format(field.name, field.area_ha))
@@ -144,12 +147,14 @@ class FarmParcel(models.Model):
     area_ha = fields.Float('Hectares', digits='Hectare', tracking=True)
     boundary = fields.GeoPolygon('Boundary', srid=4326, gist_index=True)
     has_boundary = fields.Boolean('Has Boundary',
-                                  computed='_compute_has_boundary',
+                                  compute='_compute_has_boundary',
                                   default=False)
     farm_id = fields.Many2one('agri.farm',
                               'Farm',
                               ondelete='cascade',
                               required=True)
+    farm_version_id = fields.Many2one(related='farm_id.farm_version_id',
+                                      readonly=True)
     land_cover_ids = fields.One2many(comodel_name='agri.land.cover',
                                      inverse_name='farm_parcel_id',
                                      string='Land Cover',
@@ -182,7 +187,7 @@ class FarmVersion(models.Model):
     _check_company_auto = True
 
     name = fields.Char('Name',
-                       computed='_compute_name',
+                       compute='_compute_name',
                        readonly=True,
                        stored=True)
     date = fields.Date('Date',
@@ -190,34 +195,59 @@ class FarmVersion(models.Model):
                        required=True)
     partner_id = fields.Many2one('res.partner',
                                  string='Partner',
-                                 ondelete='cascade')
+                                 ondelete='cascade',
+                                 required=True)
     company_id = fields.Many2one('res.company',
                                  required=True,
                                  default=lambda self: self.env.company)
-    parent_farm_version_id = fields.Many2one('agri.farm.version',
-                                             'Parent Version')
-    child_farm_version_ids = fields.Many2many('agri.farm.version',
-                                              'agri_farm_version_rel',
-                                              'farm_version_id',
-                                              'child_id',
-                                              string='Child Versions')
+    parent_farm_version_id = fields.Many2one(
+        'agri.farm.version',
+        'Preceding Version',
+        domain="[('partner_id', '=', partner_id)]")
+    child_farm_version_ids = fields.Many2many(
+        'agri.farm.version',
+        'agri_farm_version_rel',
+        'farm_version_id',
+        'child_id',
+        string='Following Versions',
+        domain="[('partner_id', '=', partner_id), ('id', '!=', id)]")
     farm_ids = fields.One2many(comodel_name='agri.farm',
                                inverse_name='farm_version_id',
                                string='Farms',
                                copy=True)
 
     @api.depends('date')
+    @api.onchange('date')
     def _compute_name(self):
         for farm_version in self:
-            farm_version.name = 'Version from {}'.format(
+            farm_version.name = 'As of {}'.format(
                 format_date(self.env, farm_version.date))
 
+    @api.model
+    def create(self, vals):
+        farm_version = super().create(vals)
+        if farm_version.parent_farm_version_id:
+            default = {'farm_version_id': farm_version.id}
+            farm_commands = []
+            for farm in farm_version.parent_farm_version_id.farm_ids:
+                farm_copy = farm.copy(default)
+                farm_commands += [(4, farm_copy.id, 0)]
+            farm_version.write({'farm_ids': farm_commands})
+            farm_version.parent_farm_version_id.write(
+                {'child_farm_version_ids': [(4, farm_version.id, 0)]})
+            latest_farm_version = self.env['agri.farm.version'].search(
+                [('partner_id', '=', farm_version.partner_id.id)],
+                limit=1,
+                order='date DESC')
+            farm_version.partner_id.write(
+                {'farm_version_id': latest_farm_version.id})
+        return farm_version
+
+    @api.returns('self', lambda value: value.id)
     def copy(self, default=None):
         self.ensure_one()
         default = dict(default or {}, parent_farm_version_id=self)
-        farm_version = super(FarmVersion, self).copy(default=default)
-        self.write({'child_farm_version_ids': [(4, [farm_version.id])]})
-        return farm_version
+        return super(FarmVersion, self).copy(default)
 
 
 class FarmLandUse(models.Model):
