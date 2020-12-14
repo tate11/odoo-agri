3d8bad368ea0921cac8de77ea82add231007fb76
Ross Savage <ross.savage@agrista.com>
2020-12-14 09:24:23 +0100

    [IMP] agri_commodity: Add reset button, grading line column totals

--- agri_commodity/models/agri_delivery.py
+++ agri_commodity/models/agri_delivery.py
@@ -10,7 +10,10 @@ class Delivery(models.Model):
     _order = 'name, create_date desc'
     _check_company_auto = True
 
-    name = fields.Char(string='Name', default='New', readonly=True, copy=False)
+    name = fields.Char(compute='_compute_name',
+                       string='Name',
+                       store=True,
+                       copy=False)
     partner_id = fields.Many2one('res.partner',
                                  string='Partner',
                                  states={'draft': [('readonly', False)]},
@@ -22,10 +25,12 @@ class Delivery(models.Model):
                                  default=lambda self: self.env.company)
     advance_payment_date = fields.Date(string='Advance Payment Date',
                                        states={'draft': [('readonly', False)]},
+                                       readonly=True,
                                        copy=False,
                                        tracking=True)
     delivery_date = fields.Date(string='Delivery Date',
                                 states={'draft': [('readonly', False)]},
+                                readonly=True,
                                 required=True,
                                 copy=False,
                                 tracking=True)
@@ -68,21 +73,23 @@ class Delivery(models.Model):
         domain="[('partner_id', '=', destination_partner_id)]",
         states={'draft': [('readonly', False)]},
         readonly=True)
-    product_id = fields.Many2one('product.product',
-                                 string='Product',
-                                 domain="[('is_agri_commodity', '=', True), ('categ_id.is_agri', '=', True)]",
-                                 states={'draft': [('readonly', False)]},
-                                 readonly=True,
-                                 required=True)
-    grading_id = fields.Many2one(
-        'agri.grading',
-        string='Grading',
-        ondelete='set null',
-        states={'done': [('readonly', True)]},
-        readonly=False)
+    product_id = fields.Many2one(
+        'product.product',
+        string='Product',
+        domain=
+        "[('is_agri_commodity', '=', True), ('categ_id.is_agri', '=', True)]",
+        states={'draft': [('readonly', False)]},
+        readonly=True,
+        required=True)
+    grading_id = fields.Many2one('agri.grading',
+                                 string='Grading',
+                                 ondelete='set null',
+                                 states={'done': [('readonly', True)]},
+                                 readonly=False)
     grading_product_qty = fields.Float(string='Grading Quantity',
                                        related='grading_id.product_qty')
     grading_line_ids = fields.One2many(related='grading_id.grading_line_ids',
+                                       states={'done': [('readonly', True)]},
                                        readonly=False)
     transport_partner_id = fields.Many2one(
         'res.partner',
@@ -99,66 +106,85 @@ class Delivery(models.Model):
         tracking=True,
     )
 
+    @api.depends('state')
+    @api.onchange('state')
+    def _compute_name(self):
+        for delivery in self:
+            if delivery.state == 'draft':
+                delivery.name = 'Draft'
+            elif not delivery.name or delivery.name == 'Draft':
+                seq_no = self.env['ir.sequence'].next_by_code('agri.delivery')
+                delivery.name = seq_no
+
     @api.depends('sale_order_id')
     @api.onchange('sale_order_id')
     def _compute_product(self):
-        for line in self:
-            products = line.sale_order_id.order_line.filtered(lambda order_line: order_line.product_id.is_agri)
+        for delivery in self:
+            products = delivery.sale_order_id.order_line.filtered(
+                lambda order_line: order_line.product_id.is_agri)
             if len(products) > 0:
-                line.product_id = products[0]
+                delivery.product_id = products[0]
 
     @api.depends('product_id')
     @api.onchange('product_id')
     def _compute_delivered_mass_uom(self):
-        for line in self:
-            line.delivered_mass_uom_id = line.product_id.uom_id
+        for delivery in self:
+            delivery.delivered_mass_uom_id = delivery.product_id.uom_id
 
     @api.onchange('product_id', 'delivered_mass', 'delivered_mass_uom_id')
     def _compute_grading(self):
-        for line in self:
-            if line.product_id and line.delivered_mass_uom_id and line.delivered_mass > 0:
-                if (line.grading_id and line.grading_id.product_tmpl_id.id !=
-                        line.product_id.id):
-                    line_data = line.copy_data({'grading_id': False})
-                    line.grading_id.unlink()
-                    line.write(line_data[0])
-                if (not line.grading_id and line.product_id.is_agri_commodity):
+        for delivery in self:
+            if (delivery.product_id and delivery.delivered_mass_uom_id
+                    and delivery.delivered_mass > 0):
+                if (delivery.grading_id
+                        and delivery.grading_id.product_tmpl_id.id !=
+                        delivery.product_id.id):
+                    delivery_data = delivery.copy_data({'grading_id': False})
+                    delivery.grading_id.unlink()
+                    delivery.write(delivery_data[0])
+                if (not delivery.grading_id
+                        and delivery.product_id.is_agri_commodity):
                     vals = {
-                        'product_qty': line.delivered_mass,
-                        'product_uom_id': line.delivered_mass_uom_id.id
+                        'product_qty': delivery.delivered_mass,
+                        'product_uom_id': delivery.delivered_mass_uom_id.id
                     }
-                    if line.product_id.default_grading_id:
-                        line.grading_id = line.product_id.default_grading_id.copy(
+                    if delivery.product_id.default_grading_id:
+                        delivery.grading_id = delivery.product_id.default_grading_id.copy(
                             vals)
                     else:
-                        vals.update(product_tmpl_id=line.product_id.id)
-                        line.grading_id = self.env['agri.grading'].create(vals)
+                        vals.update(product_tmpl_id=delivery.product_id.id)
+                        delivery.grading_id = self.env['agri.grading'].create(
+                            vals)
 
     @api.depends('delivered_mass', 'delivered_mass_uom_id', 'grading_id')
     @api.onchange('delivered_mass', 'delivered_mass_uom_id')
     def _compute_product_qty(self):
-        for line in self:
-            if line.grading_id and line.delivered_mass_uom_id and line.delivered_mass > 0:
-                line.grading_id.product_qty = line.delivered_mass
-                line.grading_id.product_uom_id = line.delivered_mass_uom_id
-                line.grading_product_qty = line.grading_id.product_qty
+        for delivery in self:
+            if (delivery.grading_id and delivery.delivered_mass_uom_id
+                    and delivery.delivered_mass > 0):
+                delivery.grading_id.product_qty = delivery.delivered_mass
+                delivery.grading_id.product_uom_id = delivery.delivered_mass_uom_id
+                delivery.grading_product_qty = delivery.grading_id.product_qty
                 line_commands = []
-                for grading_line in line.grading_line_ids:
-                    grading_line._compute_product_qty(line.grading_id.product_qty)
+                for grading_line in delivery.grading_line_ids:
+                    grading_line._compute_product_qty(
+                        delivery.grading_id.product_qty)
                     line_commands += [(1, grading_line.id, {
                         'product_qty': grading_line.product_qty,
                         'price': grading_line.price,
                     })]
-                line.grading_line_ids = line_commands
-                line.grading_id.grading_line_ids = line.grading_line_ids
+                delivery.grading_line_ids = line_commands
+                delivery.grading_id.grading_line_ids = delivery.grading_line_ids
 
     def action_deliver(self):
         for delivery in self:
-            seq_no = self.env['ir.sequence'].next_by_code('agri.delivery')
             delivery.state = 'delivered'
-            delivery.name = seq_no
             delivery.message_subscribe([delivery.partner_id.id])
 
     def action_done(self):
         for delivery in self:
             delivery.state = 'done'
+
+    def action_reset(self):
+        for delivery in self:
+            delivery.state = 'draft'
